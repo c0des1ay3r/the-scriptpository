bcc056b5fa5c1163750351530ca9b141ed02bbf8
c0des1ay3r <68708999+c0des1ay3r@users.noreply.github.com>
2023-05-22 10:01:14 -0500

    Delete toggleEditMode.js

--- toggleEditMode.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  if (document.body.contentEditable === "true") {
-    document.body.contentEditable = "false";
-    document.designMode = "off";
-  } else {
-    document.body.contentEditable = "true";
-    document.designMode = "on";
-  }
-}());
